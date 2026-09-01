2bfe858cad3c3f040cbb818ef735a5bdbe41dcec
asaromichael <asaro.michael@student.ccm.edu>
2026-09-01 16:59:11 -0400

    started InterestEarned

--- InterestEarned.java
+++ InterestEarned.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 public class InterestEarned {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the Principal Amount.");
+        double principal = scanner.nextDouble();
 
 
 
